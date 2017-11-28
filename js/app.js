75ef9685184154c8e858662355534bd8772004cd
eliza29 <elizaseguraf@gmail.com>
2017-11-28 16:01:02 -0500

    colocando graficos superiores

--- js/app.js
+++ js/app.js
@@ -3,9 +3,19 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//console.log(data);
 // carga la visualización API
 /*
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 */
+window.addEventListener('load',function(){
+
+
+
+
+
+
+
+
+})
