c91ee98bf795e848ca66fc36e5536262333d9aba
Nathoriz <nathortiz_41@hotmail.com>
2017-11-29 08:53:58 -0500

    obteniemdo datos

--- js/app.js
+++ js/app.js
@@ -9,9 +9,14 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 */
-window.addEventListener('load',function(){
-
-
+window.addEventListener('load', function() {
+/* Obteniendo datos de alumnos matriculadoss */
+  var activeStudents = data['AQP']['2016-2']['students'];
+  var counter = 0;
+  for (var i = 0; i < activeStudents.length; i++)
+    if (activeStudents[i] === true) {
+      counter ++;
+    }
 
 
 
